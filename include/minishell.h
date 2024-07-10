041a531f510d6a9435daddec810c9809a6c34c43
kyeh <kyeh@student.42.fr>
2024-07-10 18:23:56 +0200

    1

--- include/minishell.h
+++ include/minishell.h
@@ -15,17 +15,11 @@
 # define MINISHELL_H
 
 # include "libft.h"
-# include "ft_printf.h"
-# include "get_next_line.h"
-# include <stdlib.h>
-# include <stdio.h>
 # include <sys/types.h>
 # include <signal.h>
-# include <unistd.h>
 # include <dirent.h>
 # include <string.h>
 # include <errno.h>
-# include <fcntl.h>
 
 typedef struct		s_sh
 {
