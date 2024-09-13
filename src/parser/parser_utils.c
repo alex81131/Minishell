8aa640be04c7fc2abc1b5e3141f485642cba94c7
Kai-Erh Yeh <kyeh@made-f0Br8s5.clusters.42paris.fr>
2024-09-13 16:29:25 +0200

    fixed ps_strjoin

--- src/parser/parser_utils.c
+++ src/parser/parser_utils.c
@@ -6,9 +6,10 @@
 /*   By: kyeh <kyeh@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 16:25:30 by kyeh              #+#    #+#             */
-/*   Updated: 2024/09/05 16:26:45 by kyeh             ###   ########.fr       */
+/*   Updated: 2024/09/13 16:29:10 by kyeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #include "minishell.h"
 
@@ -24,4 +25,19 @@ int	ps_check_all_null(t_token *token)
 		token = token->next;
 	}
 	return (i);
+}
+
+char	*ps_strjoin(char *s1, char *s2)
+{
+	char	*new;
+
+	if (!s1 && !s2)
+		return (NULL);
+	else if (!s2)
+		return (s1);
+	else if (!s1)
+		return (ft_strdup(s2));
+	new = ft_strjoin(s1, s2);
+	free(s1);
+	return (new);
 }
